bfa4b08aa9e2fd4dd26cceed297d41acaa17341d
Vansh Arora <vansh.vansharora2003@gmail.com>
2026-04-08 22:14:05 +0530

    submission compliant

--- server/app.py
+++ server/app.py
@@ -46,9 +46,5 @@ def main(host: str = "0.0.0.0", port: int = 8000):
     uvicorn.run(app, host=host, port=port)
 
 
-if __name__ == "__main__":
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--port", type=int, default=8000)
-    args = parser.parse_args()
-    main(port=args.port)
+if __name__ == '__main__':
+    main()
